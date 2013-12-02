5a481c5e3ce6216b885e197e2d27dba43e59c138
RTK <rouvenkruse@web.de>
2013-12-02 20:34:01 +0100

    initial settings

--- CMS/index.php
+++ CMS/index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            print ( 'It\'s my code! It\'s my Code! My Code on the highway' );
-        ?>
-    </body>
-</html>
+<?php
+
+/*
+ * -CMS-
+ * created: 02/11/2013
+ * author: Dennis Dobslaf
+ */
