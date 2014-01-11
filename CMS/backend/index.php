07dafa2bf073313d3bdce212ef58cc230fc1f9d4
Jonas Schürmann <jonasschuermann@aol.de>
2014-01-11 20:04:33 +0100

    Issue #5: $_GET[page]-Parameter wird jetzt überprüft.

--- CMS/backend/index.php
+++ CMS/backend/index.php
@@ -8,8 +8,14 @@ if(empty($_GET['page'])){
     $_GET['page'] = 'home';
 }
 
-require_once('core/mvc/controller/'.$_GET['page'].'.php');
+$controllerPath = 'core/mvc/controller/'.$_GET['page'].'.php';
+if(!file_exists($controllerPath) || substr($_GET['page'], 0, 3) == "../"){ // $_GET['page'] darf nicht mit "../" anfangen, da sonst im Verzeichnisbaum nach oben navigiert werden könnte und man so PHP-Dateien außerhalb des controller-Ordners einbinden könnte (Sicherheitslücke)
+    header('HTTP/1.0 404 Not Found');
+    die("Diese Seite existiert nicht.");
+}
 
+require_once($controllerPath);
+    
 $classname = "ITC\\CMS\\c_".$_GET['page'];
 $controller = new $classname();
 
