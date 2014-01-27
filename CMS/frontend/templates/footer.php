abd450e23b4de2c3f1e77ad762af70a30b5ec5e9
RTK <rouvenkruse@web.de>
2014-01-27 20:13:35 +0100

    Koks am Berg

--- CMS/frontend/templates/footer.php
+++ CMS/frontend/templates/footer.php
@@ -3,5 +3,21 @@
         <footer>
             
         </footer>
+        <script src="frontend/js/jquery.js"></script>
+        <script src="frontend/js/letItSnow.js"></script>
+        <script>
+  $('header').letItSnow({
+   frames:60,
+   amount:100,
+   form: 1,
+   width: window.innerWidth,
+   height: window.innerHeight,
+   speed: [10, 100],
+   clear: true,
+   css: { position: 'absolute', border: '0', top: '0', left: '0', zIndex: '0' },
+   flakes: ['frontend/img/snow.png'],
+   radius: [12, 2]
+   });
+        </script>
     </body>
 </html>
